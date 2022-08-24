d4ea7b44b3c6d5253349af1b39b7320b6a61d308
Patryk <patryk.ties@lovehomeswap.com>
2022-08-24 13:43:26 +0100

    remove solutions

--- starter/isJSONsolution.ts
+++
@@ -1,7 +0,0 @@
-type JSONPrimitive = string | number | boolean | null;
-type JSONObject = { [k: string]: JSONValue };
-type JSONArray = JSONValue[];
-type JSONValue = JSONArray | JSONObject | JSONPrimitive;
-
-////// DO NOT EDIT ANY CODE BELOW THIS LINE //////
-function isJSON(arg: JSONValue) {}
